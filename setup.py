108f7bd91f3420571b8c5a66d08b39554bfa4dc0
André Anjos <rabello@pcuw32.cern.ch>
2010-02-15 17:03:23 +0100

    Upgrade to 0.3.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 setup(
 
     name = "audit",
-    version = "0.3.4", 
+    version = "0.3.5", 
     packages = find_packages(),
 
     # we also need all translation files and templates
